3278be864bd6030c1458181b002d6b833225c175
wekingchen <weking.chen@gmail.com>
2020-09-02 21:03:23 +0800

    Update userProfile.js

--- src/render/userProfile.js
+++ src/render/userProfile.js
@@ -1,9 +1,9 @@
 export const userProfile = {
   // title for HEAD
-  title: `Beet's OneDrive Index`,
+  title: `OneDrive Index`,
 
   // title for Header nav
-  navTitle: `👏 Beet's OneDrive Index`,
+  navTitle: `👏 OneDrive Index`,
 
   // a quick intro
   introContent: `<p>Thanks: <a href="https://github.com/spencerwooo/onedrive-cf-index">onedrive-cf-index</a></p><p style="color: #F56476">A quick demostration to make OneDrive CN(世纪互联) possible:<a href="https://github.com/beetcb/onedrive-cf-index">onedrive-cf-index-CN</a>
